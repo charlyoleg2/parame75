65c3efe898e9f5dc21bd6c0ed64bc31434a74dd2
charlyoleg <charles.braquet@gmx.fr>
2026-09-06 21:58:13 +0200

    ushell under construction

--- pkg/desi75/src/myGroup1/ushelf.ts
+++ pkg/desi75/src/myGroup1/ushelf.ts
@@ -25,7 +25,7 @@ import {
 	//radToDeg,
 	ffix,
 	pNumber,
-	pCheckbox,
+	//pCheckbox,
 	//pDropdown,
 	pSectionSeparator,
 	EExtrude,
@@ -41,8 +41,8 @@ const pDef: tParamDef = {
 		pNumber('H1', 'mm', 100, 1, 4000, 1),
 		pNumber('H2', 'mm', 340, 1, 4000, 1),
 		pNumber('H3', 'mm', 150, 1, 4000, 1),
+		pNumber('H4', 'mm', 150, 1, 4000, 1),
 		pNumber('W1', 'mm', 250, 1, 4000, 1),
-		pCheckbox('mid', true),
 		pSectionSeparator('details'),
 		pNumber('E1', 'mm', 20, 1, 400, 1),
 		pNumber('E2', 'mm', 30, 1, 400, 1),
@@ -53,14 +53,14 @@ const pDef: tParamDef = {
 	paramSvg: {
 		L1: 'ushelf_face.svg',
 		H1: 'ushelf_face.svg',
-		H2: 'bookshelf_side.svg',
-		H3: 'bookshelf_top.svg',
-		W1: 'bookshelf_top.svg',
-		mid: 'bookshelf_top.svg',
-		E1: 'bookshelf_top.svg',
-		E2: 'bookshelf_top.svg',
-		W2: 'bookshelf_top.svg',
-		H5: 'bookshelf_top.svg',
+		H2: 'ushelf_side.svg',
+		H3: 'ushelf_top.svg',
+		H4: 'ushelf_top.svg',
+		W1: 'ushelf_top.svg',
+		E1: 'ushelf_top.svg',
+		E2: 'ushelf_top.svg',
+		W2: 'ushelf_top.svg',
+		H5: 'ushelf_top.svg',
 		W5: 'ushelf_face.svg'
 	},
 	sim: {
@@ -74,48 +74,26 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
 	const figFace = figure();
 	const figSide = figure();
-	const figSideMid = figure();
+	const figSideR = figure();
 	const figTop = figure();
-	const figPlateau = figure();
-	const figBeamFace = figure();
 	const figBeamTop = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
-		const Htot1 = param.H1 + param.H2 + param.H3 + 2 * param.E1;
-		const Htot = Htot1 + param.E1;
-		const L12 = param.L1 / 2 - param.E1 / 2;
+		const Htot1 = param.H1 + param.E1 + param.H2;
+		const HtotR = Htot1 + param.H3;
+		const Htot = HtotR + param.H4;
 		const xx1 = [0, param.L1 - param.E1];
 		const xx2 = [param.E1, param.L1 - param.E1 - param.E2];
-		const xx3 = [param.E1];
-		const H22 = param.H1 + param.E1 + param.H2;
-		const H32 = H22 + param.E1 + param.H3;
-		const yy1 = [param.H1, H22];
-		const yy2 = [param.H1 - param.E2, H22 - param.E2, H32 - param.E2];
-		if (param.mid) {
-			xx1.push(L12);
-			xx2.push(L12 - param.E2);
-			xx2.push(L12 + param.E1);
-			xx3.push(L12 + param.E1);
-		}
 		const Lback = param.L1 - 2 * param.E1;
 		const Hback = Htot1 - param.H5;
-		const Lplateau1 = param.L1 - 2 * param.E1;
-		const Lplateau2 = (param.L1 - 3 * param.E1) / 2;
-		const Lplateau = param.mid ? Lplateau2 : Lplateau1;
-		const Wplateau = param.W5 + param.W1 - param.E1;
-		const LHorBeam = Lplateau - 2 * param.E2;
-		const LVerBeam = [param.H2 - param.E2, param.H3 - param.E2];
+		const Lplateau = Lback;
 		const Wtot = param.W5 + param.W1;
+		const Wplateau = Wtot - param.E1;
+		const LHorBeam = Lplateau - 2 * param.E2;
 		// step-5 : checks on the parameter values
 		if (param.H1 < param.H5 + param.E2) {
 			throw `err096: H1 ${param.H1} is too small compare to E2 ${param.E2} and H5 ${param.H5}`;
-		}
-		if (LVerBeam[0] < 0) {
-			throw `err102: H2 ${param.H2} is too small compare to E2 ${param.E2}`;
-		}
-		if (LVerBeam[1] < 0) {
-			throw `err105: H3 ${param.H2} is too small compare to E2 ${param.E2}`;
 		}
 		if (LHorBeam < 0) {
 			throw `err108: LHorBeam ${LHorBeam} is too small compare to E2 ${param.E2}`;
@@ -123,121 +101,65 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		// step-6 : any logs
 		rGeome.logstr += `Htotal ${ffix(Htot)}  Wtotal ${ffix(Wtot)} mm\n`;
 		rGeome.logstr += `BOM ${ffix(param.E1)} mm\n`;
-		rGeome.logstr += `plate-top ${ffix(param.L1)} x ${ffix(Wtot)} mm  x1\n`;
 		rGeome.logstr += `plate-back ${ffix(Lback)} x ${ffix(Hback)} mm  x1\n`;
-		rGeome.logstr += `plateau ${ffix(Lplateau)} x ${ffix(Wplateau)} mm  x${param.mid ? 4 : 2}\n`;
-		rGeome.logstr += `plate-side ${ffix(Htot1)} x ${ffix(Wtot)} mm  x1\n`;
-		rGeome.logstr += `plate-sideMid ${ffix(Htot1)} x ${ffix(Wtot - param.E1)} mm  x${param.mid ? 1 : 0}\n`;
+		rGeome.logstr += `plateau ${ffix(Lplateau)} x ${ffix(Wplateau)} mm  x1\n`;
+		rGeome.logstr += `plate-sideL ${ffix(Htot)} x ${ffix(Wtot)} mm  x1\n`;
+		rGeome.logstr += `plate-sideR ${ffix(Htot1 + param.H3)} x ${ffix(Wtot)} mm  x1\n`;
 		rGeome.logstr += `BOM ${ffix(param.E2)} x ${ffix(param.E2)} mm\n`;
-		rGeome.logstr += `beam-Y ${ffix(param.W2)} mm  x${param.mid ? 12 : 6}\n`;
-		rGeome.logstr += `beam-X ${ffix(LHorBeam)} mm  x${param.mid ? 6 : 3}\n`;
-		rGeome.logstr += `beam-Z1 ${ffix(LVerBeam[0])} mm  x${param.mid ? 4 : 2}\n`;
-		rGeome.logstr += `beam-Z2 ${ffix(LVerBeam[1])} mm  x${param.mid ? 4 : 2}\n`;
+		rGeome.logstr += `beam-Y ${ffix(param.W2)} mm  x2\n`;
+		rGeome.logstr += `beam-X ${ffix(LHorBeam)} mm  x1\n`;
 		// step-7 : drawing of the figures
 		// figFace
-		figFace.addSecond(ctrRectangle(0, Htot1, param.L1, param.E1));
+		figFace.addSecond(ctrRectangle(param.E1, param.H1, Lplateau, param.E1));
 		figFace.addMainO(ctrRectangle(param.E1, param.H5, Lback, Hback));
-		for (const ix of xx1) {
-			figFace.addSecond(ctrRectangle(ix, 0, param.E1, Htot1));
-		}
+		figFace.addSecond(ctrRectangle(xx1[0], 0, param.E1, Htot));
+		figFace.addSecond(ctrRectangle(xx1[1], 0, param.E1, HtotR));
 		for (const ix of xx2) {
-			for (const iy of yy2) {
-				figFace.addSecond(ctrRectangle(ix, iy, param.E2, param.E2));
-			}
-		}
-		for (const ix of xx3) {
-			for (const iy of yy1) {
-				figFace.addSecond(ctrRectangle(ix, iy, Lplateau, param.E1));
-			}
+			figFace.addSecond(ctrRectangle(ix, param.H1 - param.E2, param.E2, param.E2));
 		}
 		// figSide
-		figSide.addSecond(ctrRectangle(0, Htot1, Wtot, param.E1));
-		figSide.addSecond(ctrRectangle(0, param.H5, param.E1, Htot1 - param.H5));
-		const ctrSide = contour(Wtot, 0).addSegStrokeR(0, Htot1).addSegStrokeR(-Wtot, 0);
-		if (param.H5 > 0 && param.W5 > 0) {
-			ctrSide
-				.addSegStrokeR(0, -Htot1 + param.H5)
-				.addSegStrokeR(param.W5, 0)
-				.addSegStrokeR(0, -param.H5);
-		} else {
-			ctrSide.addSegStrokeR(0, -Htot1);
+		figSide.addSecond(ctrRectangle(0, param.H5, param.E1, Hback));
+		function ctrSide(iH: number): tContour {
+			const rCtr = contour(Wtot, 0).addSegStrokeR(0, iH).addSegStrokeR(-Wtot, 0);
+			if (param.H5 > 0 && param.W5 > 0) {
+				rCtr.addSegStrokeR(0, -iH + param.H5)
+					.addSegStrokeR(param.W5, 0)
+					.addSegStrokeR(0, -param.H5);
+			} else {
+				rCtr.addSegStrokeR(0, -iH);
+			}
+			rCtr.closeSegStroke();
+			return rCtr;
 		}
-		ctrSide.closeSegStroke();
-		figSide.addMainO(ctrSide);
-		for (const iy of yy1) {
-			figSide.addSecond(ctrRectangle(param.E1, iy, Wplateau, param.E1));
-		}
-		figSide.addSecond(ctrRectangle(param.E1, yy1[0] + param.E1, param.E2, LVerBeam[0]));
-		figSide.addSecond(ctrRectangle(param.E1, yy1[1] + param.E1, param.E2, LVerBeam[1]));
-		for (const iy of yy2) {
-			figSide.addSecond(ctrRectangle(param.E1, iy, param.W2, param.E2));
-		}
-		// figSideMid
-		figSideMid.mergeFigure(figSide, true);
-		const ctrSideMid = contour(Wtot, 0)
-			.addSegStrokeR(0, Htot1)
-			.addSegStrokeR(-Wtot + param.E1, 0);
-		if (param.H5 > 0 && param.W5 > param.E1) {
-			ctrSideMid
-				.addSegStrokeR(0, -Htot1 + param.H5)
-				.addSegStrokeR(param.W5 - param.E1, 0)
-				.addSegStrokeR(0, -param.H5);
-		} else {
-			ctrSideMid.addSegStrokeR(0, -Htot1);
-		}
-		ctrSideMid.closeSegStroke();
-		figSideMid.addMainO(ctrSideMid);
+		figSide.addMainO(ctrSide(Htot));
+		figSide.addSecond(ctrRectangle(param.E1, param.H1, Wplateau, param.E1));
+		figSide.addSecond(ctrRectangle(param.E1, param.H1 - param.E2, param.W2, param.E2));
+		// figSideR
+		figSideR.mergeFigure(figSide, true);
+		figSideR.addMainO(ctrSide(HtotR));
 		// figTop
-		figTop.addMainO(ctrRectangle(0, 0, param.L1, Wtot));
+		figTop.addMainO(ctrRectangle(param.E1, param.E1, Lplateau, Wplateau));
 		figTop.addSecond(ctrRectangle(param.E1, 0, Lback, param.E1));
 		figTop.addSecond(ctrRectangle(xx1[0], 0, param.E1, Wtot));
 		figTop.addSecond(ctrRectangle(xx1[1], 0, param.E1, Wtot));
-		if (param.mid) {
-			figTop.addSecond(ctrRectangle(xx1[2], param.E1, param.E1, Wtot - param.E1));
-		}
-		for (const ix of xx3) {
-			figTop.addSecond(ctrRectangle(ix + param.E2, param.E1, LHorBeam, param.E2));
-		}
-		// figPlateau
-		figPlateau.mergeFigure(figTop, true);
-		for (const ix of xx3) {
-			figPlateau.addMainO(ctrRectangle(ix, param.E1, Lplateau, Wtot - param.E1));
-		}
-		// figBeamFace
-		figBeamFace.mergeFigure(figFace, true);
-		const beamFace: tContour[] = [];
-		for (const ix of xx3) {
-			for (const iy of yy2) {
-				beamFace.push(ctrRectangle(ix + param.E2, iy, LHorBeam, param.E2));
-			}
-		}
-		for (const ix of xx2) {
-			beamFace.push(ctrRectangle(ix, yy1[0] + param.E1, param.E2, LVerBeam[0]));
-			beamFace.push(ctrRectangle(ix, yy1[1] + param.E1, param.E2, LVerBeam[1]));
-		}
-		for (const iCtr of beamFace) {
-			figFace.addSecond(iCtr);
-			figBeamFace.addMainO(iCtr);
-		}
+		figTop.addSecond(ctrRectangle(param.E1 + param.E2, param.E1, LHorBeam, param.E2));
 		// figBeamTop
 		figBeamTop.mergeFigure(figTop, true);
 		const beamTop: tContour[] = [];
 		for (const ix of xx2) {
 			beamTop.push(ctrRectangle(ix, param.E1, param.E2, param.W2));
 		}
+		beamTop.push(ctrRectangle(param.E1 + param.E2, param.E1, LHorBeam, param.E2));
 		for (const iCtr of beamTop) {
 			figTop.addSecond(iCtr);
-			figPlateau.addSecond(iCtr);
 			figBeamTop.addMainO(iCtr);
 		}
 		// final figure list
 		rGeome.fig = {
 			faceFace: figFace,
 			faceSide: figSide,
-			faceSideMid: figSideMid,
+			faceSideR: figSideR,
 			faceTop: figTop,
-			facePlateau: figPlateau,
-			faceBeamFace: figBeamFace,
 			faceBeamTop: figBeamTop
 		};
 		// volume
@@ -255,30 +177,6 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 				length: param.E1,
 				rotate: [0, 0, 0],
 				translate: [0, 0, Htot1]
-			});
-			partList.push(eName);
-		}
-		if (param.E1 > 0) {
-			const eName = `subpax_${designName}_plateau1`;
-			partExtrude.push({
-				outName: eName,
-				face: `${designName}_facePlateau`,
-				extrudeMethod: EExtrude.eLinearOrtho,
-				length: param.E1,
-				rotate: [0, 0, 0],
-				translate: [0, 0, param.H1]
-			});
-			partList.push(eName);
-		}
-		if (param.E1 > 0) {
-			const eName = `subpax_${designName}_plateau2`;
-			partExtrude.push({
-				outName: eName,
-				face: `${designName}_facePlateau`,
-				extrudeMethod: EExtrude.eLinearOrtho,
-				length: param.E1,
-				rotate: [0, 0, 0],
-				translate: [0, 0, H22]
 			});
 			partList.push(eName);
 		}
@@ -306,39 +204,15 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			});
 			partList.push(eName);
 		}
-		if (param.mid) {
-			const eName = `subpax_${designName}_sideMid`;
-			partExtrude.push({
-				outName: eName,
-				face: `${designName}_faceSideMid`,
-				extrudeMethod: EExtrude.eLinearOrtho,
-				length: param.E1,
-				rotate: [pi2, 0, pi2],
-				translate: [L12, 0, 0]
-			});
-			partList.push(eName);
-		}
 		if (param.E2 > 0) {
-			const eName = `subpax_${designName}_beam1`;
-			partExtrude.push({
-				outName: eName,
-				face: `${designName}_faceBeamFace`,
-				extrudeMethod: EExtrude.eLinearOrtho,
-				length: param.E2,
-				rotate: [pi2, 0, 0],
-				translate: [0, param.E2 + param.E1, 0]
-			});
-			partList.push(eName);
-		}
-		for (const [idx, iy] of yy2.entries()) {
-			const eName = `subpax_${designName}_beam2${idx}`;
+			const eName = `subpax_${designName}_beam`;
 			partExtrude.push({
 				outName: eName,
 				face: `${designName}_faceBeamTop`,
 				extrudeMethod: EExtrude.eLinearOrtho,
 				length: param.E2,
 				rotate: [0, 0, 0],
-				translate: [0, 0, iy]
+				translate: [0, 0, param.H1 - param.E2]
 			});
 			partList.push(eName);
 		}
